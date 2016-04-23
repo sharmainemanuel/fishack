9490ceaed7a0d2c5a8ebc89c818a71716de690ce
sharmainemanuel <manuelsharmaine@gmail.com>
2016-04-23 17:57:27 +0800

    1.12

--- js/userweather.js
+++ js/userweather.js
@@ -1,1 +1,136 @@
-function initialize(a,r,t){var n=[["Zamboanga",6.916228,122.069782],["Metro Davao",7.100497,125.608841],["Valencia City",7.865434,125.169273],["Cagayan De Oro",8.456392,124.627907],["Tacloban City",11.255724,124.95094],["Metro Cebu",10.308863,123.915028],["Iloilo City",10.720089,122.560381],["Puerto Princesa",9.9474401,118.5145237],["Legazpi City",13.1212078,123.6330866],["Lipa City",13.944612,121.0981374],["Tagaytay City",14.11722,120.8988919],["Olongapo City",14.882156,120.2759178],["Baguio City",16.3995547,120.5537555],["Laoag City",18.1981728,120.5276349],["Tuguegarao City",17.6022296,121.6894235],["Metro Manila",14.5655922,120.8845468]];vImageURL="img/weather-icons/partly_cloudy_skies.png";var e=setMyMapStyle(),o=new google.maps.StyledMapType(e,{name:"Styled Map"}),i={zoom:t,center:new google.maps.LatLng(a,r),disableDefaultUI:!0,mapTypeControlOptions:{mapTypeIds:[google.maps.MapTypeId.ROADMAP,"map_style"]}};map=new google.maps.Map(document.getElementById("map-canvas"),i),map.mapTypes.set("map_style",o),map.setMapTypeId("map_style");var s,d,g=new google.maps.InfoWindow;for(d=0;d<n.length;d++)s=new google.maps.Marker({position:new google.maps.LatLng(n[d][1],n[d][2]),map:map,icon:arrIcon1[d]}),google.maps.event.addListener(s,"click",function(a,r){return function(){var t="<table border='1'><tr><td colspan='5'><strong>"+arrCityName[r]+"</strong><br />Wind Strenght: "+arrWindStr[r]+"<br />Wind Direction: "+arrWindDir[r]+"</td></tr>";t+="<td><img src="+arrIcon1[r]+"><br /><span>"+arrMin1[r]+" - "+arrMax1[r]+"&degC</span></td>",t+="<td><img src="+arrIcon2[r]+"><br /><span>"+arrMin2[r]+" - "+arrMax2[r]+"&degC</span></td>",t+="<td><img src="+arrIcon3[r]+"><br /><span>"+arrMin3[r]+" - "+arrMax3[r]+"&degC</span></td>",t+="<td><img src="+arrIcon4[r]+"><br /><span>"+arrMin4[r]+" - "+arrMax4[r]+"&degC</span></td>",t+="<td><img src="+arrIcon5[r]+"><br /><span>"+arrMin5[r]+" - "+arrMax5[r]+"&degC</span></td>",g.setContent(t),g.open(map,a)}}(s,d))}var vLat=0,vLong=0,map,radarOverlay,imageBounds={north:0,south:0,east:0,west:0},vImageURL,arrCityName=[];arrWindStr=[],arrWindDir=[];var arrIcon1=[],arrMin1=[],arrMax1=[],arrIcon2=[],arrMin2=[],arrMax2=[],arrIcon3=[],arrMin3=[],arrMax3=[],arrIcon4=[],arrMin4=[],arrMax4=[],arrIcon5=[],arrMin5=[],arrMax5=[];$(document).ready(function(){document.getElementById("map-canvas").style.width="100%",document.getElementById("map-canvas").style.height="100%",navigator.geolocation?(vLat=12.33024,vLong=122.23156,$.ajax({type:"GET",url:"http://iligtas.ph/agaptest/outlook.php",async:!1,success:function(a){console.log(a);for(var r=0;16>r;r++)arrCityName.push(a.result.data[r].cityName),arrWindStr.push(a.result.data[r].windStr),arrWindDir.push(a.result.data[r].windDir),arrIcon1.push("img/weather-icons/day"+a.result.data[r].day1.logo+".png"),arrMin1.push(a.result.data[r].day1.min),arrMax1.push(a.result.data[r].day1.max),arrIcon2.push("img/weather-icons/day"+a.result.data[r].day2.logo+".png"),arrMin2.push(a.result.data[r].day2.min),arrMax2.push(a.result.data[r].day2.max),arrIcon3.push("img/weather-icons/day"+a.result.data[r].day3.logo+".png"),arrMin3.push(a.result.data[r].day3.min),arrMax3.push(a.result.data[r].day3.max),arrIcon4.push("img/weather-icons/day"+a.result.data[r].day4.logo+".png"),arrMin4.push(a.result.data[r].day4.min),arrMax4.push(a.result.data[r].day4.max),arrIcon5.push("img/weather-icons/day"+a.result.data[r].day5.logo+".png"),arrMin5.push(a.result.data[r].day5.min),arrMax5.push(a.result.data[r].day5.max)}}),navigator.geolocation.getCurrentPosition(function(a){vLat=a.coords.latitude,vLong=a.coords.longitude,console.log(vLat+", "+vLong),google.maps.event.addDomListener(window,"load",initialize(vLat,vLong,10))})):console.log("error in getting location")});
+var vLat=0, vLong=0;
+var map;
+var radarOverlay;
+var imageBounds = {north: 0, south: 0, east: 0, west: 0};
+var vImageURL;
+var arrCityName=[]; arrWindStr=[], arrWindDir=[];
+var arrIcon1=[], arrMin1=[], arrMax1=[];
+var arrIcon2=[], arrMin2=[], arrMax2=[];
+var arrIcon3=[], arrMin3=[], arrMax3=[];
+var arrIcon4=[], arrMin4=[], arrMax4=[];
+var arrIcon5=[], arrMin5=[], arrMax5=[];
+
+$(document).ready(function()
+{
+    //var scrheight = screen.height - (screen.height * .5);
+    document.getElementById('map-canvas').style.width="100%";
+    document.getElementById('map-canvas').style.height="100%"; //scrheight + "px";
+    if (!navigator.geolocation) 
+    {
+      console.log("error in getting location");
+    } else {
+      // Philippines 12.330240, 122.231560
+      vLat = 12.330240; vLong = 122.231560;
+      
+      
+      $.ajax({
+        type: "GET",
+        url: "http://iligtas.ph/agaptest/outlook.php",
+        async: false,
+        success: function(myData){
+          console.log(myData);
+          for(var i = 0; i < 16; i++){
+          arrCityName.push(myData.result.data[i].cityName);
+          arrWindStr.push(myData.result.data[i].windStr);
+          arrWindDir.push(myData.result.data[i].windDir);
+
+          //first info
+          arrIcon1.push("img/weather-icons/day" + myData.result.data[i].day1.logo + ".png");
+          arrMin1.push(myData.result.data[i].day1.min);
+          arrMax1.push(myData.result.data[i].day1.max);
+
+          //second info
+          arrIcon2.push("img/weather-icons/day" + myData.result.data[i].day2.logo + ".png");
+          arrMin2.push(myData.result.data[i].day2.min);
+          arrMax2.push(myData.result.data[i].day2.max);
+
+          //third info
+          arrIcon3.push("img/weather-icons/day" + myData.result.data[i].day3.logo + ".png");
+          arrMin3.push(myData.result.data[i].day3.min);
+          arrMax3.push(myData.result.data[i].day3.max);
+
+          //fourth info
+          arrIcon4.push("img/weather-icons/day" + myData.result.data[i].day4.logo + ".png");
+          arrMin4.push(myData.result.data[i].day4.min);
+          arrMax4.push(myData.result.data[i].day4.max);
+
+          //fifth info
+          arrIcon5.push("img/weather-icons/day" + myData.result.data[i].day5.logo + ".png");
+          arrMin5.push(myData.result.data[i].day5.min);
+          arrMax5.push(myData.result.data[i].day5.max);
+          
+        }}
+      });
+      navigator.geolocation.getCurrentPosition(function(position){
+      vLat=position.coords.latitude; 
+      vLong=position.coords.longitude;
+      console.log(vLat + ", " + vLong);
+       google.maps.event.addDomListener(window, 'load',initialize(vLat, vLong, 10));
+    });
+      //google.maps.event.addDomListener(window, 'load',initialize(vLat, vLong, 6));
+    }
+});
+
+
+function initialize(vLat, vLong, vZoom)
+{
+ var arrLocations =[
+    ['Zamboanga', 6.916228, 122.069782],
+    ['Metro Davao', 7.100497, 125.608841],
+    ['Valencia City', 7.865434, 125.169273],
+    ['Cagayan De Oro', 8.456392, 124.627907],
+    ['Tacloban City', 11.255724, 124.950940],
+    ['Metro Cebu', 10.308863, 123.915028],
+    ['Iloilo City', 10.720089, 122.560381],
+    ['Puerto Princesa', 9.9474401,118.5145237],
+    ['Legazpi City', 13.1212078,123.6330866],
+    ['Lipa City', 13.944612,121.0981374],
+    ['Tagaytay City', 14.11722,120.8988919],
+    ['Olongapo City', 14.882156,120.2759178],
+    ['Baguio City', 16.3995547,120.5537555],
+    ['Laoag City', 18.1981728,120.5276349],
+    ['Tuguegarao City', 17.6022296,121.6894235],
+    ['Metro Manila', 14.5655922,120.8845468]
+  ];
+  vImageURL = "img/weather-icons/partly_cloudy_skies.png";
+  var myMapStyle = setMyMapStyle();
+  var mapReference = new google.maps.StyledMapType(myMapStyle, {name: "Styled Map"});
+  var mapOptions = 
+    {
+      zoom: vZoom,
+      center: new google.maps.LatLng(vLat, vLong),
+      //mapTypeId: google.maps.MapTypeId.HYBRID,
+      disableDefaultUI: true,
+      mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, "map_style"]}
+    };
+  var positionstring;
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  map.mapTypes.set("map_style", mapReference);
+  map.setMapTypeId("map_style");
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker, i;
+
+  for(i=0; i<arrLocations.length; i++)
+  {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(arrLocations[i][1], arrLocations[i][2]),
+      map:map,
+      icon: arrIcon1[i]
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i){
+    return function(){
+      var vLayout = "<table border='1'><tr><td colspan='5'><strong>"+arrCityName[i]+"</strong><br />Wind Strenght: "+arrWindStr[i]+"<br />Wind Direction: "+arrWindDir[i]+"</td></tr>";
+      vLayout += "<td><img src="+arrIcon1[i]+"><br /><span>"+arrMin1[i]+" - "+arrMax1[i]+"&degC</span></td>";
+      vLayout += "<td><img src="+arrIcon2[i]+"><br /><span>"+arrMin2[i]+" - "+arrMax2[i]+"&degC</span></td>";
+      vLayout += "<td><img src="+arrIcon3[i]+"><br /><span>"+arrMin3[i]+" - "+arrMax3[i]+"&degC</span></td>";
+      vLayout += "<td><img src="+arrIcon4[i]+"><br /><span>"+arrMin4[i]+" - "+arrMax4[i]+"&degC</span></td>";
+      vLayout += "<td><img src="+arrIcon5[i]+"><br /><span>"+arrMin5[i]+" - "+arrMax5[i]+"&degC</span></td>";
+      infowindow.setContent(vLayout);
+      infowindow.open(map, marker);
+    }
+  })(marker, i));
+  }
+}
